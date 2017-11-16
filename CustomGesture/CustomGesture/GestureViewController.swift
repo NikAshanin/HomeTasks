e0eab3b7a11a8f2f41b0e5eaa25f00a75bf66779
kurilkin <alexeykurilkin@gmail.com>
2017-11-16 21:25:50 +0300

    Transfer to HomeTasks repo

--- CustomGesture/CustomGesture/GestureViewController.swift
+++ CustomGesture/CustomGesture/GestureViewController.swift
@@ -1,5 +1,17 @@
 import UIKit
 
 final class GestureViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addGestureRecognizer(OLetterRecognizer(target: self, action: #selector(oLetterRecognized)))
+    }
+    @objc func oLetterRecognized() {
+        guard let tabBar = storyboard?.instantiateViewController(withIdentifier: "TabBarController")
+            else {
+                return
+        }
+        tabBar.modalTransitionStyle = .crossDissolve
+        present(tabBar, animated: true)
+    }
 
 }
