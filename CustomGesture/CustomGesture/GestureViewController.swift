390bebde2114b52f35a67b7d4032914130e8e4e7
Ira <ira@MacBook-Air-ira.local>
2017-11-19 17:05:07 +0300

    add gesture
    add gesture

--- CustomGesture/CustomGesture/GestureViewController.swift
+++ CustomGesture/CustomGesture/GestureViewController.swift
@@ -1,5 +1,18 @@
 import UIKit
 
 final class GestureViewController: UIViewController {
+    @IBOutlet weak private var imageView: UIImageView!
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = #imageLiteral(resourceName: "circle.jpg")
+        imageView.contentMode = .scaleAspectFit
+        let oRecognizer = ORecognizer (midPoint: self.view.center, target: self, action: #selector(oRecognized))
+        view.addGestureRecognizer(oRecognizer)
+    }
+    @objc private func oRecognized() {
+        let storyboard = UIStoryboard(name: "TabBar", bundle: nil)
+        let homeViewController = storyboard.instantiateViewController(withIdentifier: "tabBar")
+        self.present(homeViewController, animated: true, completion: nil)
+    }
 }
