7f74fbcc86a08eeb8b53db234f4d980b50a6a9ee
Sitora <redastify@gmail.com>
2017-11-20 23:34:37 +0300

    fixed some mistakes

--- CustomGesture/CustomGesture/GestureViewController.swift
+++ CustomGesture/CustomGesture/GestureViewController.swift
@@ -1,13 +1,15 @@
 import UIKit
 
 final class GestureViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
         let oRecognizer = ORecognizer(target: self,
                                       action: #selector(ORecognized))
         view.addGestureRecognizer(oRecognizer)
     }
-    @objc func ORecognized() {
-        self.performSegue(withIdentifier: "goToTabBarController", sender: self)
+
+    @objc private func ORecognized() {
+        performSegue(withIdentifier: "goToTabBarController", sender: self)
     }
 }
