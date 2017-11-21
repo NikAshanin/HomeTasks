bd99b7eed6e49a8e097bbc28232f642061746af0
Gregory Metelev <metelev@tutanota.de>
2017-11-21 12:49:54 +0300

    First CustomGesture commit

---
+++ CustomGesture/CustomGesture/TabBarController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
