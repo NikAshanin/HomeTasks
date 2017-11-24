97f0f29f9914486438e59770a513b768129ec91e
Sitora <redastify@gmail.com>
2017-11-24 19:30:40 +0300

    Revert "Remove unused code from starwars"
    This reverts commit a13468d57624486d6e5565329bf98407c571f56d.

--- StarWars/StarWars/ViewController.swift
+++ StarWars/StarWars/ViewController.swift
@@ -1,5 +1,25 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Vyacheslav Beltyukov on 11/16/17.
+//  Copyright © 2017 EPAM. All rights reserved.
+//
+
 import UIKit
 
 class ViewController: UIViewController {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
 }
+
