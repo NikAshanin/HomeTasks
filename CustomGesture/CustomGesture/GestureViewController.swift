77006aa832aab12b411478f79ce23d7e6ad4080b
Sitora <redastify@gmail.com>
2017-11-24 19:33:19 +0300

    Revert "custom gesture hometask"
    This reverts commit 3e5513417e6ed388e3f9268d54f930f4bd6c572a.

--- CustomGesture/CustomGesture/GestureViewController.swift
+++ CustomGesture/CustomGesture/GestureViewController.swift
@@ -2,14 +2,4 @@ import UIKit
 
 final class GestureViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let oRecognizer = ORecognizer(target: self,
-                                      action: #selector(ORecognized))
-        view.addGestureRecognizer(oRecognizer)
-    }
-
-    @objc private func ORecognized() {
-        performSegue(withIdentifier: "goToTabBarController", sender: self)
-    }
 }
