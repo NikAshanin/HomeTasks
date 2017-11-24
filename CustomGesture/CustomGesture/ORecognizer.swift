b49c998987e5c1a76b2a4964e65096990e7810eb
Sitora <redastify@gmail.com>
2017-11-24 19:27:25 +0300

    fixed problem with branches

--- CustomGesture/CustomGesture/ORecognizer.swift
+++ CustomGesture/CustomGesture/ORecognizer.swift
@@ -3,9 +3,17 @@ import UIKit.UIGestureRecognizerSubclass
 
 class ORecognizer: UIGestureRecognizer {
     // MARK: magic numbers
+    // strokePrecision - допускаемая погрешность (отклонение)
+    private let strokePrecision: CGFloat = 20
+    // mathPrecision - допускаемая погрешность вычислений
+    private let mathPrecision: CGFloat = 10.5
     // strokePart - этап выполнения
     private var strokePart: UInt = 0
     private var firstTap: CGPoint?
+    // центр круга, значения x  и  y замерены
+    private let centerPoint = CGPoint(x: 197, y: 320)
+    // a - радиус по оси x, b - радиус по оси y
+    private let xRadius: CGFloat = 160, yRadius: CGFloat = 160
 
     override func reset() {
         super.reset()
@@ -21,14 +29,12 @@ class ORecognizer: UIGestureRecognizer {
         }
         firstTap = touches.first?.location(in: view?.superview)
         guard let firstTap = firstTap,
-            abs(firstTap.x - ParamsForCalculating.centerPoint.x) <= ParamsForCalculating.strokePrecision
-                && abs(firstTap.y - ParamsForCalculating.centerPoint.y
-                    + ParamsForCalculating.xRadius) <= ParamsForCalculating.strokePrecision else {
+            abs(firstTap.x - centerPoint.x) <= strokePrecision
+                && abs(firstTap.y - centerPoint.y + xRadius) <= strokePrecision else {
                     return
         }
-        guard abs(firstTap.x - ParamsForCalculating.centerPoint.x) <= ParamsForCalculating.strokePrecision
-            && abs(firstTap.y - ParamsForCalculating.centerPoint.y
-                + ParamsForCalculating.xRadius) <= ParamsForCalculating.strokePrecision else {
+        guard abs(firstTap.x - centerPoint.x) <= strokePrecision
+            && abs(firstTap.y - centerPoint.y + xRadius) <= strokePrecision else {
                 state = .failed
                 return
         }
@@ -57,26 +63,23 @@ class ORecognizer: UIGestureRecognizer {
             return
         }
         if strokePart == 0
-            && abs(ParamsForCalculating.centerPoint.x + ParamsForCalculating.xRadius
-                - currentPoint.x) <= ParamsForCalculating.strokePrecision
+            && abs(centerPoint.x + xRadius - currentPoint.x) <= strokePrecision
             && isPointOnRadius(currentPoint) {
             print("part 1 complete")
             strokePart = 1
         } else if strokePart == 1
-            && abs(ParamsForCalculating.centerPoint.y + ParamsForCalculating.yRadius
-                - currentPoint.y) <= ParamsForCalculating.strokePrecision
+            && abs(centerPoint.y + yRadius - currentPoint.y) <= strokePrecision
             && isPointOnRadius(currentPoint) {
             strokePart = 2
             print("part 2 complete")
         } else if strokePart == 2
-            && abs(ParamsForCalculating.centerPoint.x - ParamsForCalculating.xRadius
-                - currentPoint.x) <= ParamsForCalculating.strokePrecision
+            && abs(centerPoint.x - xRadius - currentPoint.x) <= strokePrecision
             && isPointOnRadius(currentPoint) {
             strokePart = 3
             print("part 3 complete")
         } else if strokePart == 3
-            && abs(currentPoint.y - firstTap.y) <= ParamsForCalculating.strokePrecision
-            && abs(currentPoint.x - firstTap.x) <= ParamsForCalculating.strokePrecision
+            && abs(currentPoint.y - firstTap.y) <= strokePrecision
+            && abs(currentPoint.x - firstTap.x) <= strokePrecision
             && isPointOnRadius(currentPoint) {
             strokePart = 4
             state = .recognized
@@ -87,7 +90,7 @@ class ORecognizer: UIGestureRecognizer {
     // MARK: formula
     // x^2/a^2 + y^2/b^2 = 1
     func isPointOnRadius(_ currentPoint: CGPoint) -> Bool {
-        return abs(pow(currentPoint.x, 2) / pow(ParamsForCalculating.xRadius, 2) +
-            pow(currentPoint.y, 2) / pow(ParamsForCalculating.yRadius, 2) - 1) <= ParamsForCalculating.mathPrecision
+        return abs(pow(currentPoint.x, 2) / pow(xRadius, 2) +
+            pow(currentPoint.y, 2) / pow(yRadius, 2) - 1) <= mathPrecision
     }
 }
