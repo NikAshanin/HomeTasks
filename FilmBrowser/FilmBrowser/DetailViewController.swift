4570dcc89908b609642bdbb686e4258bedc90b69
Михаил Агеев <kylesp@rambler.ru>
2017-11-26 15:44:42 +0300

    ready for check

--- FilmBrowser/FilmBrowser/DetailViewController.swift
+++ FilmBrowser/FilmBrowser/DetailViewController.swift
@@ -1,9 +1,29 @@
 import UIKit
 
-final class DetailViewController: UIViewController {
+class DetailViewController: UIViewController {
+  
+  @IBOutlet private weak var imageFilm: UIImageView!
+  @IBOutlet private weak var button: UIButton!
+  @IBOutlet private weak var descriptionLabel: UILabel!
+  @IBOutlet private weak var countLikesLabel: UILabel!
+  @IBOutlet private weak var nameOfFilmLabel: UILabel!
+  weak var delegate: LikeChangeProtocol?
+  var film = Film()
+  var currentIndex = Int()
+  @IBAction func submit(_ sender: Any) {
+    film.countLikes += 1
+    delegate?.likeChange(currentIndex)
+    countLikesLabel.text = "Всего лайков:\(String(film.countLikes))"
+  }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    descriptionLabel.text = film.description
+    imageFilm.image = UIImage(named: film.urlImage)
+    nameOfFilmLabel.text = film.name
+    countLikesLabel.text = "Всего лайков:\(String(film.countLikes))"
+  }
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
+protocol LikeChangeProtocol: class {
+  func likeChange(_ index: Int)
 }
---
+++ FilmBrowser/FilmBrowser/Film.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+struct Film {
+  var name: String = ""
+  var description: String = ""
+  var countLikes: Int = 0
+  var urlImage: String = ""
+}
+class ArrayFilms {
+  private var films: [Film] = []
+  func pushFilm(name: String, description: String, urlImage: String) {
+    var film = Film()
+      film.name = name
+      film.description = description
+      film.countLikes = Int(arc4random() % 1000)
+      film.urlImage = urlImage
+      films.append(film)
+  }
+  func get(index: Int) -> Film {
+    return films[index]
+  }
+  func count() -> Int {
+    return films.count
+  }
+  func changeLikes(_ index: Int) {
+    films[index].countLikes += 1
+  }
+}
