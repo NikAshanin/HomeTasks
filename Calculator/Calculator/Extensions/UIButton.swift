0bdede9a7d58663ac3ba212104ec274d9fbf37b0
Юрий Сорокин <u40@Urijs-MacBook-Pro.local>
2017-11-30 20:26:49 +0300

    fix

--- Calculator/Calculator/Extensions/UIButton.swift
+++ Calculator/Calculator/Extensions/UIButton.swift
@@ -9,10 +9,7 @@ public extension UIButton {
             return ""
         }
         set {
-            if newValue == "white" {
-                setBackgroundImage(UIImage(color: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), for: .selected)
-            }
-
+            setBackgroundImage(colorName: newValue, for: .selected)
         }
     }
 
@@ -22,15 +19,19 @@ public extension UIButton {
             return ""
         }
         set {
-            if newValue == "white" {
-                setBackgroundImage(UIImage(color: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), for: .highlighted)
-            }
-            if newValue == "gray" {
-                setBackgroundImage(UIImage(color: #colorLiteral(red: 0.6509147286, green: 0.6510263681, blue: 0.6509000063, alpha: 1)), for: .highlighted)
-            }
-            if newValue == "black" {
-                setBackgroundImage(UIImage(color: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)), for: .highlighted)
-            }
+            setBackgroundImage(colorName: newValue, for: .highlighted)
+        }
+    }
+    
+    private func setBackgroundImage(colorName: String, for state: UIControlState) {
+        if colorName == "white" {
+            setBackgroundImage(UIImage(color: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), for: state)
+        }
+        if colorName == "gray" {
+            setBackgroundImage(UIImage(color: #colorLiteral(red: 0.6509147286, green: 0.6510263681, blue: 0.6509000063, alpha: 1)), for: state)
+        }
+        if colorName == "black" {
+            setBackgroundImage(UIImage(color: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)), for: state)
         }
     }
 }
