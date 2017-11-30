c48419834513ae454da89388b6f1fcd170c868d7
Alexander <mail10101011@gmail.com>
2017-11-30 22:56:17 +0300

    Added OLetterRecognizer.swift, CircleView.swift
    OLetterRecognizer.swift содержит код описание жеста.
    CircleView.swift содержит код описания круга, отображаемого на экране.

---
+++ CustomGesture/CustomGesture/CircleView.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+@IBDesignable
+class CircleView: UIView {
+    // MARK: properties
+    private var scale: CGFloat = 0.8
+    var circleCenter: CGPoint {
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+
+    var circleRadius: CGFloat {
+        return min(bounds.size.width, bounds.size.height) / 2 * scale
+    }
+
+    private func pathForCircle() -> UIBezierPath {
+        let path = UIBezierPath(arcCenter: circleCenter,
+                                radius: circleRadius,
+                                startAngle: 0,
+                                endAngle: 2 * CGFloat.pi,
+                                clockwise: true)
+        print("Current radius is: \(circleRadius) with center in: \(circleCenter)")
+        path.lineWidth = 5.0
+        return path
+    }
+    override func draw(_ rect: CGRect) {
+        UIColor.black.set()
+        pathForCircle().stroke()
+    }
+}
