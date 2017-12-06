3295685d260268580239482ffd1f18070f1111a6
Sitora <redastify@gmail.com>
2017-12-06 20:46:52 +0300

    small change

--- CustomGesture/CustomGesture/Extensions/CGPoint+isPointOnRadius.swift
+++ CustomGesture/CustomGesture/Extensions/CGPoint+isPointOnRadius.swift
@@ -2,8 +2,9 @@ import UIKit
 extension CGPoint {
     // MARK: formula
     // x^2/a^2 + y^2/b^2 = 1
-    static func isPointOnRadius(_ currentPoint: CGPoint) -> Bool {
-        return abs(pow(currentPoint.x, 2) / pow(ParamsForCalculating.xRadius, 2) +
-            pow(currentPoint.y, 2) / pow(ParamsForCalculating.yRadius, 2) - 1) <= ParamsForCalculating.mathPrecision
+
+    var isOnRadius: Bool {
+        return abs(pow(x, 2) / pow(ParamsForCalculating.xRadius, 2) +
+            pow(y, 2) / pow(ParamsForCalculating.yRadius, 2) - 1) <= ParamsForCalculating.mathPrecision
     }
 }
