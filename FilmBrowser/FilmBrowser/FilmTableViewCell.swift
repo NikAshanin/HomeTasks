f0e1c6d500dba5988a5aac6930d1e4b196adb517
Vsevolod <oneactiondrama@gmail.com>
2017-12-07 20:36:29 +0300

    initial commit film browser

--- FilmBrowser/FilmBrowser/FilmTableViewCell.swift
+++ FilmBrowser/FilmBrowser/FilmTableViewCell.swift
@@ -1,5 +1,25 @@
 import UIKit
 
-final class FilmTableViewCell: UITableViewCell {
+final class FilmTableCell: UITableViewCell {
 
+    @IBOutlet private weak var filmLogo: UIImageView!
+    @IBOutlet private weak var filmName: UILabel!
+    @IBOutlet private weak var likesCount: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    func configureCell(_ cellViewModel: FilmCellViewModel) {
+
+        guard let logo = cellViewModel.filmLogo,
+            let name = cellViewModel.filmName,
+            let likes = cellViewModel.likesCount
+            else { return }
+
+        filmLogo.image = UIImage(named: logo)
+        filmName.text = name
+        likesCount.text = String(likes)
+
+    }
 }
