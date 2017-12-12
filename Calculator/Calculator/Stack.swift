47c999ef2f94634777e448d13d1bd3cb86085cf0
Gregory Metelev <metelev@tutanota.de>
2017-12-12 20:38:56 +0300

    First commit

---
+++ Calculator/Calculator/Stack.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+struct Stack<Element> {
+    fileprivate var array: [Element] = []
+    fileprivate var writeIndex: Int = 0
+
+    mutating func push(_ element: Element) {
+        if writeIndex < array.count {
+            array.removeSubrange((writeIndex)...(array.count-1))
+        }
+        array.append(element)
+        writeIndex += 1
+    }
+
+    mutating func pop() -> Element? {
+        if array.isEmpty || (writeIndex == 0) {
+            return nil
+        }
+        writeIndex -= 1
+        let elem = array[writeIndex]
+        return elem
+    }
+
+    mutating func unPop() -> Element? {
+        if (writeIndex + 1) <= array.count {
+            writeIndex += 1
+        }
+        return array[writeIndex-1]
+    }
+
+    mutating func clear() {
+        array.removeAll()
+        writeIndex = 0
+    }
+
+    func top() -> Element? {
+        return array.last
+    }
+
+    func isEmpty() -> Bool {
+        return writeIndex == 0
+    }
+
+    func size() -> Int {
+        return array.count
+    }
+}
