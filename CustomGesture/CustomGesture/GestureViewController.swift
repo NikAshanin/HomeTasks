cf0a3734e2743113d4e1b8e8fec9a6143c444abc
Tatiana Cherednichenko <tatinacher@gmail.com>
2017-12-19 16:27:54 +0300

    fixed

--- CustomGesture/CustomGesture/GestureViewController.swift
+++ CustomGesture/CustomGesture/GestureViewController.swift
@@ -6,15 +6,16 @@ final class GestureViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let circleWidth = circle.frame.size.width/2
-        let circleHeigh = circle.frame.size.height/2
+        let circleHeight = circle.frame.size.height/2
         var circleCenter = circle.frame.origin
         circleCenter.x += circleWidth
-        circleCenter.y += circleHeigh
+        circleCenter.y += circleHeight
         let oRecognizer = OGestureRecognizer(width: circleWidth,
-                                             height: circleHeigh,
+                                             height: circleHeight,
                                              center: circleCenter,
                                              target: self,
                                              action: #selector(oRecognized))
+
         view.addGestureRecognizer(oRecognizer)
     }
 
