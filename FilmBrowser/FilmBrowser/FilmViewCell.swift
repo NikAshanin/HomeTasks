f2e0fdf2577b5c1fb746c2a94e9f551f6fcd1deb
Gregory Metelev <metelev@tutanota.de>
2017-12-19 15:02:56 +0300

    File renamed, Unused file was deleted

--- FilmBrowser/FilmBrowser/FilmViewCell.swift
+++ FilmBrowser/FilmBrowser/FilmViewCell.swift
@@ -1,15 +1,15 @@
 import UIKit
 
 final class FilmViewCell: UITableViewCell {
-  @IBOutlet private weak var imageFilm: UIImageView!
-  @IBOutlet private weak var countLikesLabel: UILabel!
-  @IBOutlet private weak var titleFilmLabel: UILabel!
+    @IBOutlet private weak var imageFilm: UIImageView!
+    @IBOutlet private weak var countLikesLabel: UILabel!
+    @IBOutlet private weak var titleFilmLabel: UILabel!
 
     public func setImage(image: UIImage?) {
         guard image != nil else {
             return
         }
-       imageFilm.image = image
+        imageFilm.image = image
     }
 
     public func setFilmTitle(title: String) {
