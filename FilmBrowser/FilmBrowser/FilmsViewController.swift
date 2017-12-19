67a5b1ecc963fd09be49b5028336569fafe014a7
Gregory Metelev <metelev@tutanota.de>
2017-12-19 14:48:14 +0300

    film_browser added

---
+++ FilmBrowser/FilmBrowser/FilmsViewController.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+final class InfoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, LikeChangeProtocol {
+
+  private let films = FilmBuilder.naiveGenerate()
+
+  @IBOutlet private  weak var infoLabel: UILabel!
+  @IBOutlet private weak var tableView: UITableView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    tableView.estimatedRowHeight = 100
+    tableView.rowHeight = UITableViewAutomaticDimension
+  }
+
+  func likeChange(_ index: Int) {
+    films.changeLikes(index)
+    DispatchQueue.main.async {
+      self.tableView.reloadData()
+    }
+  }
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return films.count()
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? FilmViewCell
+    let film = films.get(index: indexPath.row)
+    cell?.setImage(image: UIImage(named: film.urlImage))
+    cell?.setFilmTitle(title: film.name)
+    cell?.setLikesCount(likes: film.countLikes)
+
+    return cell ?? UITableViewCell()
+  }
+
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    guard let detailViewController = storyboard?.instantiateViewController(withIdentifier: "DetailViewController")
+        as? DetailViewController
+      else {
+        return
+    }
+    detailViewController.likeChangeDelegate = self
+    detailViewController.currentIndex = indexPath.row
+    detailViewController.film = films.get(index: indexPath.row)
+
+    navigationController?.pushViewController(detailViewController, animated: true)
+  }
+}
---
+++ FilmBrowser/FilmBrowser/TableViewCell.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+final class FilmViewCell: UITableViewCell {
+  @IBOutlet private weak var imageFilm: UIImageView!
+  @IBOutlet private weak var countLikesLabel: UILabel!
+  @IBOutlet private weak var titleFilmLabel: UILabel!
+
+    public func setImage(image: UIImage?) {
+        guard image != nil else {
+            return
+        }
+       imageFilm.image = image
+    }
+
+    public func setFilmTitle(title: String) {
+        titleFilmLabel.text = title
+    }
+
+    public func setLikesCount(likes: Int) {
+        countLikesLabel.text = String(likes)
+    }
+}
