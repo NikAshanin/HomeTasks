1980121e6c89e22430f1aefa3aa23139db62b2d2
Gregory Metelev <metelev@tutanota.de>
2017-12-19 15:33:53 +0300

    Conflicts resolved

--- FilmBrowser/FilmBrowser/DetailViewController.swift
+++ FilmBrowser/FilmBrowser/DetailViewController.swift
@@ -1,9 +1,32 @@
 import UIKit
 
+protocol LikeChangeProtocol: class {
+    func likeChange(_ index: Int)
+}
+
 final class DetailViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+  @IBOutlet private weak var imageFilm: UIImageView!
+  @IBOutlet private weak var button: UIButton!
+  @IBOutlet private weak var descriptionLabel: UILabel!
+  @IBOutlet private weak var countLikesLabel: UILabel!
+  @IBOutlet private weak var nameOfFilmLabel: UILabel!
 
+  weak var likeChangeDelegate: LikeChangeProtocol?
+  var film = Film()
+  var currentIndex = Int()
+
+  @IBAction private func submit(_ sender: Any) {
+    film.countLikes += 1
+    likeChangeDelegate?.likeChange(currentIndex)
+    countLikesLabel.text = "Likes :\(String(film.countLikes))"
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    descriptionLabel.text = film.description
+    countLikesLabel.text = "Likes :\(String(film.countLikes))"
+    nameOfFilmLabel.text = film.name
+    imageFilm.image = UIImage(named: film.urlImage)
+  }
 }
